3caa62dd3fc871601153a249cd7c99233faa29a4
Khushi Mehta <khushimehta1409@gmail.com>
2026-06-18 01:51:57 +0530

    fix urls routing

--- secure_auth_system/urls.py
+++ secure_auth_system/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("🚀 Django is working on Render!")
 
 urlpatterns = [
-    path('', home),  # ROOT URL FIX
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 ]
