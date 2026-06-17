a88ec97c3ad18a159b7c3644e14ff1e0c21e5ae1
Khushi Mehta <khushimehta1409@gmail.com>
2026-06-18 01:44:03 +0530

    Fix urls

--- secure_auth_system/urls.py
+++ secure_auth_system/urls.py
@@ -1,12 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Django is working on Render!")
 
 urlpatterns = [
-    path('admin-panel/', admin.site.urls),
+    path('', home),  # ROOT URL FIX
+    path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
