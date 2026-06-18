9df2a22969ba7f86d5f7476efdc5820a57d0b6b4
Khushi Mehta <khushimehta1409@gmail.com>
2026-06-18 11:46:21 +0530

    fix homepage routing

--- secure_auth_system/urls.py
+++ secure_auth_system/urls.py
@@ -1,12 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Django is LIVE on Render")
 
 urlpatterns = [
-    path('admin-panel/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', home),   # TEMP HOME (GUARANTEED WORKING)
     path('', include('accounts.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
