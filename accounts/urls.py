8512f8dc5cc3f8ffc47622bd700306963aa049e8
Khushi Mehta <khushimehta1409@gmail.com>
2026-06-18 11:49:50 +0530

    fix homepage routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Django is LIVE on Render")
 
 urlpatterns = [
+    path('', home),   # ROOT WORKS HERE
     path('register/admin/', views.register_admin),
     path('login/admin/', views.login_admin),
+    path('register/distributor/', views.register_distributor),
+    path('login/distributor/', views.login_distributor),
 ]
--- secure_auth_system/urls.py
+++ secure_auth_system/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("🚀 Django is LIVE on Render")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),   # TEMP HOME (GUARANTEED WORKING)
     path('', include('accounts.urls')),
 ]
